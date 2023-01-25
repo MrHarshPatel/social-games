a778fafdc89b97af69947b0e29c46e7da45f7b3b
Harsh Patel <harshpatel@sahils-mbp.lan>
2023-01-24 21:34:26 -0800

    Initial commit.

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const next = require('next');
+
+const PORT = 3001;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    const server = express();
+
+    // Express routes go here
+
+    server.get('*', (req, res) => {
+        return handle(req, res);
+    });
+
+    server.listen(PORT, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${PORT}`);
+    });
+});
