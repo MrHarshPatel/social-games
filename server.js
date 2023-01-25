8b1fa387cd22e77307b979a960d46a018ff9dab3
Harsh Patel <harshpatel@sahils-mbp.lan>
2023-01-25 11:16:00 -0800

    Added full leave and re-join as new user functionality.

--- server.js
+++ server.js
@@ -47,9 +47,46 @@ const User = mongoose.model('User', userSchema);
 
 app.prepare().then(() => {
     const server = express();
+    const http = require('http');
+    const httpServer = http.createServer(server).listen(3001, function () {
+        console.log("Express server listening on port " + 3001);
+    });
+    const io = require('socket.io')(httpServer,
+        {
+            cors: {
+                origin: "http://localhost:3000",
+                credentials: true
+            }
+        }
+    );
+
+    // Sockets for real-time stuff.
+    io.on('connection', (socket) => {
+        socket.on('join-lobby', async (lobbyId) => {
+            socket.join(lobbyId);
+            const lobby = await Lobby.findOne({ url: `${lobbyId}` }).populate('users');
+            io.to(lobbyId).emit('update-lobby', { lobby: lobby });
+        });
+
+        socket.on('leave-lobby', async (lobbyId, username) => {
+            socket.leave(lobbyId);
+            const lobby = await Lobby.findOne({ url: `${lobbyId}` });
+            const user = await User.findOneAndRemove({ username: `${username}` });
+            lobby.users.pull(user);
+            if (lobby.users.length < 1) {
+                await lobby.remove();
+            }
+            else {
+                await lobby.save();
+            }
+            io.to(lobbyId).emit('update-lobby', { lobby: await lobby.populate('users') });
+        });
+    });
+
     server.use(cors({ origin: "http://localhost:3000", credentials: true }))
     server.use(cookieParser());
 
+    // Routes impact the lobby SQL database.
     server.post('/api/lobbies', async (req, res) => {
         const newLobby = new Lobby({ url: generateUniqueUrl() });
         await newLobby.save();
@@ -61,6 +98,7 @@ app.prepare().then(() => {
         console.log("Pong!");
     });
 
+    // Route to add a new user if this user is new, and show the lobby.
     server.get('/api/lobbies/:id', async (req, res) => {
         const lobbyId = req.params.id;
         const lobby = await Lobby.findOne({ url: `${lobbyId}` }).populate('users');
@@ -68,32 +106,28 @@ app.prepare().then(() => {
             res.status(404).send("Lobby not found");
             return;
         }
+        var addedUser = undefined;
 
-        const cookie = req.cookies[lobbyId];
+        const cookie = req.cookies[`${lobbyId}/user`];
         if (!cookie) {
-            res.cookie(lobbyId, true, { maxAge: 60 * 60 * 24 * 7, isHttpOnly: false });
             // create a new user and add them to the lobby
             const newUser = new User({ lobby: lobby._id, username: await generateRandomUsername() });
             await newUser.save();
             lobby.users.push(newUser);
             res.cookie(`${lobbyId}/user`, newUser.username, { maxAge: 60 * 60 * 24 * 7, isHttpOnly: false });
+            addedUser = newUser.username;
         }
         await lobby.save();
-        res.json({ lobby: lobby });
+        res.json({ lobby: lobby, addedUser: addedUser });
     });
 
-    server.get('/api/lobbies/:id/users', async (req, res) => {
-        const lobbyId = req.params.id;
-        const users = await User.find({ lobby: lobbyId });
-        res.json({ users });
+    // Route to remove a user's cookie.
+    server.get('/api/lobbies/:id/user/leave', async (req, res) => {
+        res.clearCookie(`${req.params.id}/user`).send();
     });
 
     server.get('*', (req, res) => {
         return handle(req, res);
     });
 
-    server.listen(3001, (err) => {
-        if (err) throw err;
-        console.log('> Ready on http://localhost:3001');
-    });
 });
--- src/pages/lobby/[lobbyId].tsx
+++ src/pages/lobby/[lobbyId].tsx
@@ -2,21 +2,61 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import ErrorPage from '../404';
+import io from 'socket.io-client';
+import Cookies from 'js-cookie';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+}));
+
+const socket = io('http://localhost:3001');
 
 export default function LobbyPage() {
+  const classes = useStyles();
   const router = useRouter();
   const { lobbyId } = router.query;
   const [users, setUsers] = useState([]);
   const [notFound, setNotFound] = useState(false);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  socket.on('update-lobby', (data) => {
+    console.log("Got an update lobby request.");
+    console.log(data.lobby.users);
+    setUsers(data.lobby.users);
+  });
 
   useEffect(() => {
-    if(!router.isReady) return;
+    if (!router.isReady) return;
     const { lobbyId } = router.query;
     const fetchUsers = async () => {
       try {
         const { data } = await axios.get(`/api/lobbies/${lobbyId}`, {
           withCredentials: true
-          });
+        });
+
+        console.log(data);
+        if (data.addedUser) {
+          console.log("NEW user joined!");
+          socket.emit('join-lobby', lobbyId);
+          setCurrentUser(data.addedUser);
+        }
+
         setUsers(data.lobby.users);
         setNotFound(false);
       } catch (error) {
@@ -32,18 +72,47 @@ export default function LobbyPage() {
     return <ErrorPage />;
   }
 
+  const handleLeaveLobby = async () => {
+    try {
+      const username = Cookies.getJSON(`${lobbyId}/user`);
+      console.log(username);
+      socket.emit('leave-lobby', lobbyId, username);
+      const { data } = await axios.get(`/api/lobbies/${lobbyId}/user/leave`, {
+        withCredentials: true
+      });
+      router.push('/');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      <h1>Users in lobby: {lobbyId}</h1>
-      {users.length > 0 ? (
-        <ul>
-          {users.map((user) => (
-            <li key={user.username}>{user.username}</li>
-          ))}
-          </ul>
-      ) : (
-        <p>No users in this lobby</p>
-      )}
+      <Typography variant="h4" gutterBottom>
+        Users in lobby: {lobbyId}
+      </Typography>
+      <List className={classes.root}>
+        {users.length >
+          0 ? (
+          users.map((user) => (
+            <ListItem key={user.username}>
+              <ListItemText primary={user.username} primaryTypographyProps={user.username === currentUser ? {className: classes.boldText} : null} />
+            </ListItem>
+          ))
+        ) : (
+          <Typography variant="body1" gutterBottom>
+            No users in this lobby
+          </Typography>
+        )}
+      </List>
+      <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        onClick={handleLeaveLobby}
+      >
+        Leave Lobby
+      </Button>
     </div>
   );
 }
